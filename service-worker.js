642fe03a9c4918f24ebb093feb42df11885e37b9
maxon4ik585-sudo <maxon4ik585@gmail.com>
2026-09-24 00:35:46 +0300

    Aptechka v1.5.2: improve natural search and PWA updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='aptechka-v1-5-1';
+const CACHE='aptechka-v1-5-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
